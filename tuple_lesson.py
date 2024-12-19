a1390b00978c038f099a701827907bd4b20da0bf
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2024-12-19 07:28:32 +0000

    new

--- tuple_lesson.py
+++ tuple_lesson.py
@@ -1,6 +1,6 @@
 """
-Name:
-Date:
+Name:Duncan Staats
+Date:12/16/2024
 Lesson: Tuples
 """
 
@@ -13,20 +13,25 @@ On Sunday I don't talk about them. :)
 # The look like an ordered pair in math
 
 # Make tuple with two items
-
+my_tuple = ("3",4)
+print(my_tuple)
 # Make tuple with one item
-
+lonely_tuple = (5,) #not a tuple without a comma and the ( )
+print(lonely_tuple)
 
 # You access elements from them using [ ] just like strings
-
-
+print(my_tuple[0])
 
 # Also like strings, you can only concatenate tuples with other tuples
-
+not_lonely_tuple = my_tuple + (5,)
+print(not_lonely_tuple)
+integer_tuple = (int(my_tuple[0]),4,5)
+print(integer_tuple)
 # I heard you like tuples...
 # so you can also put tuples inside your tuples
 # need two [] to access them.
-
+character_info = (("Rosalie",100),("Cedric",75),("Shana",94))
+print(character_info[1][0])
 
 ########## Exercise ##########
 
